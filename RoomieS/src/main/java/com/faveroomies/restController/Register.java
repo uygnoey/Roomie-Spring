537fc59fcf649580cd84e42c3bb1e506b6213d8d
YeonGyu Yang <ygyang0302@gmail.com>
2016-04-21 08:29:15 -0400

    Spring Rest Controller

---
+++ RoomieS/src/main/java/com/faveroomies/restController/Register.java
@@ -0,0 +1,25 @@
+/**
+ * <p>Project name is RoomieS<br>
+ * Package name is life.theroomie.restController<br>
+ * File name is Register.java<br>
+ *
+ * @author YeonGyu Yang
+ * @since Apr 1, 2016
+ */
+package com.faveroomies.restController;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author YeonGyu Yang
+ * @Since Apr 1, 2016
+ *
+ */
+@RestController
+@MapperScan("life.theroomie.mapper")
+public class Register {
+	
+
+}
