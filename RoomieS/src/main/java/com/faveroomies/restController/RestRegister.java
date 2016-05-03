a8faca4de66836cf8acc089e7e1fcc5d02df0b31
YeonGyu Yang <ygyang0302@gmail.com>
2016-05-02 22:25:26 -0400

    Rest Controller renamed

--- RoomieS/src/main/java/com/faveroomies/restController/RestRegister.java
+++ RoomieS/src/main/java/com/faveroomies/restController/RestRegister.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @MapperScan("life.theroomie.mapper")
-public class Register {
+public class RestRegister {
 	
 
 }
