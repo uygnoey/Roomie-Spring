8fcad2c48a98c11a6f78126ce0212e1e45dfc2cb
YeonGyu Yang <ygyang0302@gmail.com>
2016-05-05 22:42:12 -0400

    RestController

--- RoomieS/src/main/java/com/faveroomies/restController/RestRegister.java
+++ RoomieS/src/main/java/com/faveroomies/restController/RestRegister.java
@@ -8,8 +8,15 @@
  */
 package com.faveroomies.restController;
 
+import org.apache.ibatis.annotations.Param;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.faveroomies.mapper.RoomieMapper;
 
 /**
  * 
@@ -20,6 +27,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @MapperScan("life.theroomie.mapper")
 public class RestRegister {
-	
+
+	private Logger logger = LoggerFactory.getLogger(RestRegister.class);
+
+	@Autowired
+	private RoomieMapper roomieMapper;
+
+	@RequestMapping(value = "/register/overlap", params = "username")
+	public boolean usernameOverlap(@Param("username") String username) {
+
+		logger.info("Username Overlap Check");
+
+		int overlapCount = roomieMapper.countUser(username);
+
+		if (overlapCount > 0 || username.length() <= 4)
+			return true;
+		else
+			return false;
+
+	}
+
+	@RequestMapping(value = "/register/overlap", params = "email")
+	public boolean emailOverlap(@Param("email") String email) {
+
+		logger.info("Eamil Overlap Check");
+
+		int overlapCount = 0;
+
+		if (email.split("[+]").length > 1) {
+			logger.info(email.split("[+]")[0]);
+			overlapCount = roomieMapper.countEmail(email.split("[+]")[0] + "%");
+			logger.info(((Integer) overlapCount).toString());
+		} else {
+			overlapCount = roomieMapper.countEmail(email + "%");
+			logger.info(((Integer) overlapCount).toString());
+		}
+
+		if (overlapCount > 0 || email.split("[@]").length <= 1)
+			return true;
+		else
+			return false;
+
+	}
 
 }
